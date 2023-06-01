7b9b1aa72c85ddcdba18c36da962e9b5f4a5a8d9
Cooper <Cooperransom08@gmail.com>
2023-05-31 23:08:25 -0400

    get some GUI done

--- source/GUI.py
+++ source/GUI.py
@@ -3,8 +3,34 @@
 try:
   import os, sys
   import tkinter as tk
+  from helper import functions, crypto
   from parser import createSet
 except Exception as e:
   print(f'ERROR: [GUI] An error occurred when importing dependencies. \n{e}\n')
   sys.exit(1)
+  
+window = tk.Tk()
+window.geometry("600x750")
+  
+
+class utility:
+  def openNewWindow():
+     
+    # Toplevel object which will
+    # be treated as a new window
+    newWindow = tk.Toplevel(window)
+ 
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("New Window")
+ 
+    # sets the geometry of toplevel
+    newWindow.geometry("200x200")
+ 
+    # A Label widget to show in toplevel
+    tk.Label(newWindow, text ="This is a new window").pack()
+  
+
+#def menu():
+utility.openNewWindow() 
 # REFERENCE: https://github.com/itzCozi/0swald-AI, THEME: Dark(3E3E42/2D2D30/E4E6EB)
--- source/main.py
+++ source/main.py
@@ -1,8 +1,11 @@
+# AFTER COMPILED THIS FILE LAUNCHS IN WINDOW VERSION MEANING IT IS NOT ABLE TO BE SEEN BY THE USER
+
 # This file guides the user through starting a new testing session which contains of creating a question set inputting the path of said set and asking if they would like to start a session
 # In a session the set file is encoded and then that file (key at the end) would be decrypted by their Test4Py program assuming they choose the 'join test' option and entered the set path
 try:
   import os, sys
   from checks import prerun
+  from GUI import menu, error_page
   from helper import functions, crypto
 except Exception as e:
   print(f'ERROR: [MAIN] An error occurred when importing dependencies. \n{e}\n')
