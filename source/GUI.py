a6bb9a1d2ef253f4d8694cb68915af3f792391b5
Cooper <Cooperransom08@gmail.com>
2023-05-31 23:09:43 -0400

    sorry was working in local file

--- source/GUI.py
+++ source/GUI.py
@@ -3,34 +3,67 @@
 try:
   import os, sys
   import tkinter as tk
-  from helper import functions, crypto
-  from parser import createSet
 except Exception as e:
   print(f'ERROR: [GUI] An error occurred when importing dependencies. \n{e}\n')
   sys.exit(1)
   
 window = tk.Tk()
-window.geometry("600x750")
+window.title('Test4Py')
+window.geometry('850x650')
   
 
 class utility:
-  def openNewWindow():
-     
-    # Toplevel object which will
-    # be treated as a new window
-    newWindow = tk.Toplevel(window)
- 
-    # sets the title of the
-    # Toplevel widget
-    newWindow.title("New Window")
- 
-    # sets the geometry of toplevel
-    newWindow.geometry("200x200")
- 
-    # A Label widget to show in toplevel
-    tk.Label(newWindow, text ="This is a new window").pack()
-  
+  def resetAll():
+    # Reset everything
+    for widget in window.winfo_children():
+      widget.destroy()
 
-#def menu():
-utility.openNewWindow() 
+  def openNewWindow(x, y, title):
+    if not isinstance(x, int) or isinstance(y, int):
+      print('ERROR: Both x and y need to be integers.')
+      sys.exit(1)
+    newWindow = tk.Toplevel(window)
+    newWindow.title(title)
+    newWindow.geometry(f'{x}x{y}')
+
+def menu():
+  # Declare widgets
+  header = tk.Label(window, height=5, text ='Test4Py', bg='#3E3E42', fg='#E4E6EB')
+  btn1 = tk.Button(window, text ='Join A Testing Session', command = join_session(), bg='#3E3E42', fg='#E4E6EB')
+  btn2 = tk.Button(window, text ='Start New Testing Session', command = new_session(), bg='#3E3E42', fg='#E4E6EB')
+  
+  # Config widgets
+  header.config(font=('monospace', 26))
+  btn1.config(font=('monospace', 16))
+  btn2.config(font=('monospace', 16))
+  
+  # Pack widgets
+  header.pack(anchor=tk.CENTER,fill=tk.X)
+  btn1.pack(anchor=tk.CENTER,fill=tk.X)
+  btn2.pack(anchor=tk.CENTER,fill=tk.X)
+  
+  tk.mainloop()
+
+def error_page():
+  # Config widgets
+  header = tk.Label(window, height=5, text ='Whoops...', bg='#3E3E42', fg='#E4E6EB')
+  blurb = tk.Text(window, bg='#3E3E42', fg='#E4E6EB' )
+  
+  # Insertion/config widgets
+  header.config(font=('monospace', 20))
+  blurb.config(font=('monospace', 16))
+  blurb.insert(tk.END, 'An unknown error was encountered, You can exit the application and try again in a little.')
+  blurb.config(state='disabled')
+  
+  # Pack widgets
+  header.pack(anchor=tk.CENTER,fill=tk.X)
+  blurb.pack(anchor=tk.CENTER,fill=tk.Y)
+  
+  tk.mainloop()
+
+def new_session():
+  pass
+def join_session():
+  pass
+
 # REFERENCE: https://github.com/itzCozi/0swald-AI, THEME: Dark(3E3E42/2D2D30/E4E6EB)
